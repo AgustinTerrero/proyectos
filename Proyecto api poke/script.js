93695123db589e8f356f1756572637abc097dd24
AgustinTerrero <101226436+AgustinTerrero@users.noreply.github.com>
2023-09-25 10:29:35 -0300

    Add files via upload

--- Proyecto api poke/script.js
+++ Proyecto api poke/script.js
@@ -1,22 +1,55 @@
 
-for (let i = 1; i <20; i++)
-fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-        .then( res => res.json())
-        .then(json => setdata(json))
 
-        function setdata(data){
-                const cardconteiner = document.querySelector('#tarjeta')
-cardconteiner.innerHTML += `
-<div class="card">
-<img src="${data.sprites.front_default}" alt="" srcset="">
-<div class="card__content">
-  <p class="card__title">${data.name}</p>
-  <p class="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-  <button class="card__button">${data.id}</button>
-  <button class="card__button secondary">Source Code</button>
-</div>
-</div>`
+const allPokemon = [];
 
+for (let i = 1; i < 151; i++) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then(res => res.json())
+    .then(json => {
+      allPokemon.push(json);
 
+     
+      setdata(json);
+    });
 }
 
+function setdata(data) {
+  let tipos = data.types.map(type => `<p class="${type.type.name} tipo">${type.type.name}</p>`);
+  tipos = tipos.join('');
+  const cardconteiner = document.querySelector('#tarjeta');
+  cardconteiner.innerHTML += `
+    <div class="card">
+      <img src="https://play.pokemonshowdown.com/sprites/xyani/${data.name}.gif" alt="${data.name}" class="image">
+      <h1>#${data.id}</h1>
+      <div class="card-info">
+        <span>${data.name}</span>
+        <div class="pokemon-tipos">
+          ${tipos}
+        </div>
+        <div class="pokemon-stats">
+          <p class="stat">${data.height}m</p>
+          <p class="stat">${data.weight}kg</p>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+
+
+const searchInput = document.querySelector('#searchInput');
+
+searchInput.addEventListener('input', searchPokemon);
+
+function searchPokemon() {
+  const searchValue = searchInput.value.toLowerCase();
+
+  const cardconteiner = document.querySelector('#tarjeta');
+  cardconteiner.innerHTML = '';
+
+  allPokemon.forEach(pokemon => {
+    if (pokemon.name.toLowerCase().includes(searchValue)) {
+      setdata(pokemon);
+    }
+  });
+}
